d871744e57f0171aac425b2fd691b78b62a4164a
stefan-niedermann <info@niedermann.it>
2020-02-27 19:57:10 +0100

    Add JSON+LD & poster

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.Sidenav.init(document.querySelectorAll('.sidenav'));
-    M.Parallax.init(document.querySelectorAll('.parallax'));
     M.Collapsible.init(document.querySelectorAll('.collapsible'));
     if (!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
         M.Materialbox.init(document.querySelectorAll('.materialboxed'));
